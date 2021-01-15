6c51f8c3b110625a50f9b088cb3f3deee171c50c
kanon-t <kanon.tsuda16@gmail.com>
2021-01-15 23:03:37 +0000

    Fixed linting.

--- xo.py
+++ xo.py
@@ -1,4 +1,4 @@
-'''Module for naughts-and-crosses game.'''
+"""Module for naughts-and-crosses game."""
 import random
 
 import numpy as np
@@ -6,24 +6,27 @@ import numpy as np
 
 #exercise 1
 def create_board():
-    '''Creates a 3x3 naughts-and-crosses board.'''
+    """Creates a 3x3 naughts-and-crosses board."""
     return  np.zeros((3,3), dtype = int)
 
 
 #exercise 2
 def place(board, player, position):
+    """Places player in empty grid."""
     if board[position] == 0:
         board[position] = player
 
 
 #exercise 3
 def possibilities(board):
+    """Finds all empty board spaces."""
     x_coord, y_coord = np.where(board == 0)
     return [(x_coord[i], y_coord[i]) for i in range(len(x_coord))]
 
 
 #exercise 4
 def random_place(board, player):
+    """Checks for empty space on board and returns board with player placed at random location."""
     selection = possibilities(board)
     if selection:
         position = random.choice(selection)
@@ -33,22 +36,26 @@ def random_place(board, player):
 
 #exercise 6
 def row_win(board, player):
+    """Assesses congruency of player across row."""
     return np.any(np.all(board==player, axis=1))
 
 
 #exercise 7
 def col_win(board, player):
+    """Assesses congruency of player across column."""
     return np.any(np.all(board==player, axis=0))
 
 
 #exercise 8
 def diag_win(board, player):
+    """Assesses diagonal congruency of player."""
     return np.all(np.diag(board==player)) or \
         np.all(np.diag(np.fliplr(board) == player))
 
 
 #exercise 9
 def evaluate(board):
+    """Checks for any wins and returns player number; otherwise, returns -1 for a draw."""
     for player in [1, 2]:
         for function in [row_win, col_win, diag_win]:
             if function(board, player):
@@ -58,6 +65,7 @@ def evaluate(board):
 
 #exercise 11
 def play_strategic_game():
+    """Plays randomised game but with player 1 always starting in the middle, and returns winner."""
     board = create_board()
     board[1,1] = 1
     while True:
--- xo_test.py
+++ xo_test.py
@@ -1,14 +1,18 @@
-import numpy as np
+"""Test suits for naughts-and-crosses game."""
 from unittest import TestCase
+import numpy as np
 
 import xo
 
 
 class AppTestSuits(TestCase):
+    """This is a test suit class."""
     def test_create_board(self):
+        """Tests the create_board function."""
         xo.create_board()
 
     def test_player_1_win_column(self):
+        """Tests col_win function with player 1."""
         board = xo.create_board()
 
         xo.place(board, 1, (0,0))
@@ -18,6 +22,7 @@ class AppTestSuits(TestCase):
         self.assertEqual(xo.evaluate(board), 1)
 
     def test_player_2_win_row(self):
+        """Tests row_win function with player 2."""
         board = xo.create_board()
 
         board[(0,0)] = 2
@@ -27,6 +32,7 @@ class AppTestSuits(TestCase):
         self.assertEqual(xo.evaluate(board), 2)
 
     def test_player_1_win_diag_1(self):
+        """Tests diag_win function with player 1."""
         board = xo.create_board()
 
         board[(0,0)] = 1
@@ -36,6 +42,7 @@ class AppTestSuits(TestCase):
         self.assertEqual(xo.evaluate(board), 1)
 
     def test_player_1_win_diag_2(self):
+        """Tests reversed diag_win function with player 1."""
         board = xo.create_board()
 
         board[(2,0)] = 1
@@ -45,6 +52,7 @@ class AppTestSuits(TestCase):
         self.assertEqual(xo.evaluate(board), 1)
 
     def test_ongoing(self):
+        """Tests evaluate(board) function."""
         board = np.array([
             [1, 2, 1],
             [1, 1, 2],
